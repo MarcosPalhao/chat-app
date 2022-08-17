28405e223abaa6de43313a720676bbade8cc0655
MarcosPalhao <75809289+MarcosPalhao@users.noreply.github.com>
2022-08-17 07:42:09 -0300

    first commit

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,65 @@
+import 'package:chat/components/auth_form.dart';
+import 'package:chat/core/models/auth_form_data.dart';
+import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AuthPage extends StatefulWidget {
+  const AuthPage({Key? key}) : super(key: key);
+
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool _isloading = false;
+
+  Future<void> _handleSubmit(AuthFormData formData) async {
+    try {
+      setState(() => _isloading = true);
+
+      if (formData.isLogin) {
+        await AuthService().login(
+          formData.email,
+          formData.password,
+        );
+      } else {
+        await AuthService().signup(
+          formData.name,
+          formData.email,
+          formData.password,
+          formData.image,
+        );
+      }
+    } catch (error) {
+    } finally {
+      setState(() => _isloading = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Stack(
+        children: [
+          Center(
+            child: SingleChildScrollView(
+              child: AuthForm(onSubmit: _handleSubmit),
+            ),
+          ),
+          if (_isloading)
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.5),
+              ),
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
